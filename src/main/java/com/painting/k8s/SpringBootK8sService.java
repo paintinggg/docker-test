2620c02f6a28292ee20eb51b2710a6496b67622f
painting <410370847@qq.com>
2023-09-01 16:30:30 +0800

    初始化

---
+++ src/main/java/com/painting/k8s/SpringBootK8sService.java
@@ -0,0 +1,17 @@
+package com.painting.k8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Painting
+ * @date 2023年09月01日 15:46
+ */
+@SpringBootApplication
+public class SpringBootK8sService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootK8sService.class, args);
+    }
+
+}
---
+++ src/main/java/com/painting/k8s/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.painting.k8s.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Painting
+ * @date 2023年09月01日 15:42
+ */
+@Slf4j
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        log.info("接收到外界访问.");
+        return "欢迎访问K8s中的springboot项目";
+    }
+
+}
